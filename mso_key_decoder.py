b9a84307bd2f9012aabf692ef03b891268f3c90b
Nick Timkovich <prometheus235@gmail.com>
2010-11-29 22:39:25 +0000

    Updates to work with 2010, changed output format, added inverse base24 decoder (encoding yields readable code, decode converts to binary)
    git-svn-id: svn://localhost/repo1/trunk@3 331ce708-7b2c-4c0d-95b3-12965bb25856

--- mso_key_decoder.py
+++ mso_key_decoder.py
@@ -1,14 +1,18 @@
 import sys
-#import string
+import string
 import math
 import _winreg as wr
 
 #b24chrs = (string.digits + string.ascii_uppercase)[:24]
-b24chrs = '0123456789ABCDEFGHIJKLMN'
+generic_b24chrs = '0123456789ABCDEFGHIJKLMN'
 
-mskey_len = 25
-mskey_b24chrs = 'BCDFGHJKMPQRTVWXY2346789'
-mskey_reg_root = r'Software\Microsoft\Office'
+code_len = 25 # encoded key length (user-readable key)
+bin_len = 15 # binary key length
+regkey_idx = 52 # start of key in DPID for 2003, 2007
+regkey_idx_2010 = 0x328 # start in DPID for 2010
+
+b24chrs = 'BCDFGHJKMPQRTVWXY2346789'
+reg_root = r'Software\Microsoft\Office'
 
 def chunks(l, n):
     """ Yield successive n-sized chunks from l.
@@ -16,14 +20,19 @@ def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
         
-def b24decode(input, declen=None, map=None):
-    if declen is None:
+def b24encode(input, outlen=None, chrmap=None):
+
+    # The number of coded characters to actually generate can't be 
+    # determined soley from the input length, but we can guess.
+    if outlen is None:
         # each base24 code char takes ~4.585 bits (ln24 / ln2)
-        declen = int(math.ceil(8*len(input) / 4.585))
-    if map is None:
-        map = b24chrs
+        outlen = int(math.ceil(8*len(input) / 4.585))
         
-    input = [ord(i) for i in input]
+    # Use default character mapping [0-9A-N] if none provided
+    if chrmap is None:
+        chrmap = generic_b24chrs
+        
+    input = [ord(i) for i in input[::-1]]
     '''
     # takes less memory (does it piecewise), but more complex
     decoded = []
@@ -47,29 +56,70 @@ def b24decode(input, declen=None, map=None):
         enc = enc * 256 + i
         
     dec = []
-    for i in range(declen):
-        dec.append(map[enc % 24])
+    for i in range(outlen):
+        dec.append(chrmap[enc % 24])
         enc = enc // 24
         
     dec.reverse()
     return ''.join(dec)
 
-def msoKeyDecode(regkey):
+def b24decode(input, chrmap=None):
+
+    # Use default character mapping [0-9A-N] if none provided
+    if chrmap is None:
+        chrmap = generic_b24chrs
+    
+    # clean invalid characters from input
+    rmchrs = []
+    for i in xrange(256):
+        if not chr(i) in chrmap:
+            rmchrs.append(chr(i))
+            
+    tt = string.maketrans(chrmap, ''.join([chr(i) for i in xrange(24)]))
+    
+    input = input.translate(tt, ''.join(rmchrs))
+    
+    #print repr(input)
+    
+    encnum = 0
+    for cc in input:
+        encnum *= 24
+        encnum += ord(cc)
+        
+    enc = []
+    while encnum:
+        enc.append(encnum % 256)
+        encnum = encnum // 256
+    
+    return ''.join([chr(i) for i in enc])
+    
+def msoKeyDecode(regkey, version=None):
     '''Decodes a registry key value, by extracting product key 
     from bytes 52-66 and decoding.
     
-    regkey is a string containing the contents of 'DigitalProductID'
+    Office 2010 (14.0) appears to store the key at 0x328 to 0x337 in 
+    DPID.  The "Registration" tree is different (cluttered) versus 
+    other versions, and the DPID value is (exactly) 7 times longer than 
+    before (1148 bytes, up from 164).
+    
+    Tested with a 2010 full suite and a trial of Visio.
+    
+    Parameters:
+    - regkey is a string containing the contents of "DigitalProductID"
+    - version is the decimal version number given by the key directly 
+    under the "Office" key root
     '''
-    enckey = regkey[52:66+1][::-1]
+    if version is None:
+        version = 11 # (default 2003, 2007 appears to be compatible.)
+        
+    if float(version) < 14:
+        enckey = regkey[regkey_idx:regkey_idx+bin_len]
+    else:
+        enckey = regkey[regkey_idx_2010:regkey_idx_2010+bin_len]
     
-    deckey = b24decode(enckey, mskey_len, map=mskey_b24chrs)
-    
-    # translation table for MS base24 characters (eliminates possibly
-    # confused characters: I l 1, V U)
-    #tran = string.maketrans(b24chrs, mskey_b24chrs)
-    #deckey = deckey.translate(tran)
-    
-    return '-'.join([chunk for chunk in chunks(deckey,5)])
+    deckey = b24encode(enckey, code_len, chrmap=b24chrs)
+        
+    return '-'.join(list(chunks(deckey,5)))
     
 def SubKeys(key):
     i = 0
@@ -77,9 +127,7 @@ def SubKeys(key):
         try:
             subkey = wr.EnumKey(key, i)
             yield subkey
-        except WindowsError:
-            # "WindowsError: [Error 259] No more data is available"
-            # exhausted all keys in the given key, end generator
+        except WindowsError: # [Error 259] No more data is available
             break
         i += 1
         
@@ -89,43 +137,46 @@ def KeyValues(key):
         try:
             value = wr.EnumValue(key, i)
             yield value
-        except WindowsError:
-            # "WindowsError: [Error 259] No more data is available"
-            # exhaused all values in the given key, end generator
+        except WindowsError: # [Error 259] No more data is available
             break
         i += 1
     
 def main(argv=None):
+    '''Scans local Microsoft Office registry keys for DigitalProductID values
+    and encodes the binary data in base24.
+    
+    Note: The given "Name:" of Office 2010 products is incorrect
+    (may just provide a single program name), though the Product Key 
+    should be valid.
+    '''
     if argv is None:
         argv = sys.argv
-     
-    ''' # sample data
-    # HKLM/Software/Microsoft/Office/11.0/
-    #         Registration/{blahblah}/DigitalProductID
-    enc =   range(52) +            [ 15 bytes of data... ]
-    ''.join([chr(i) for i in enc])
+        
+    mso_root = wr.OpenKey(wr.HKEY_LOCAL_MACHINE, reg_root)
     
-    print msoKeyDecode(enc)
-    '''
+    rf = "{0:<40} {1:<}"
+    product_head = "Product Name"
+    dpid_head = "Digital Product ID (key encoded in base24)"
     
-    mso_root_key = wr.OpenKey(wr.HKEY_LOCAL_MACHINE, mskey_reg_root)
+    print rf.format(product_head, dpid_head)
+    print rf.format('-' * len(product_head),'-' * len(dpid_head))
     
-    for subkey in SubKeys(mso_root_key):
-        for subsubkey in SubKeys(
-                wr.OpenKey(mso_root_key, subkey)):
-            for subsubsubkey in SubKeys(
-                    wr.OpenKey(
-                    wr.OpenKey(mso_root_key,
-                               subkey), 
-                               subsubkey)):
+    for subkey in SubKeys(mso_root):
+        # subkey always observed to be a version number (11.0, 12.0, 14.0...)
+        # where a DPID will be found.
+        for sub2key in SubKeys(
+                wr.OpenKey(mso_root, subkey)):
+            # sub2key always observed to be "Registration" when DPID is found
+            for sub3key in SubKeys(
+                    wr.OpenKey(wr.OpenKey(mso_root, subkey), sub2key)):
+                # sub3key often some UUID ending in 0F:F1:CE
                 dpid_found = False
                 for keyvalue in KeyValues(
-                        wr.OpenKey(
-                        wr.OpenKey(
-                        wr.OpenKey(mso_root_key,
-                                   subkey),
-                                   subsubkey),
-                                   subsubsubkey)):
+                        wr.OpenKey(wr.OpenKey(wr.OpenKey(
+                            mso_root,
+                                subkey),
+                                sub2key),
+                                sub3key)):
                     if keyvalue[0] == 'DigitalProductID':
                         dpid_found = True
                         dpid = keyvalue
@@ -133,7 +184,12 @@ def main(argv=None):
                         name = keyvalue
                         
                 if dpid_found:
-                    print "Product Name: %s\n\t Key: %s\n" % (name[1], msoKeyDecode(dpid[1]))
+                    #print ("Product Name: %s\n"
+                    #       "         Key: %s\n") % \
+                    #      (name[1], 
+                    #       msoKeyDecode(dpid[1],subkey))
+                           
+                    print rf.format(name[1], msoKeyDecode(dpid[1],subkey))
         
 if __name__ == "__main__":
     sys.exit(main())
